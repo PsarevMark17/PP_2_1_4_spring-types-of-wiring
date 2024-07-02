ea3cf0c06cfcde7360ae1116fdddfce5d9e80dcc
PsarevMark17 <150838212+PsarevMark17@users.noreply.github.com>
2024-07-03 01:52:54 +0300

    Update Rabbit4.java

--- src/main/java/koschei/models/Rabbit4.java
+++ src/main/java/koschei/models/Rabbit4.java
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
-public class Rabbit4 { // Koschei
+public class Rabbit4 {
 
     private Duck5 duck;
 
